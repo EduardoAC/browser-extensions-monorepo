93cd2fd86ccfd10d651ac19216f3c4b29959a8d3
Eduardo <EduardoAC@users.noreply.github.com>
2023-10-08 09:41:35 +0100

    Update webpack and manifest to support extension

--- apps/react-app-using-messages-manifest-v3/webpack.config.js
+++ apps/react-app-using-messages-manifest-v3/webpack.config.js
@@ -1,9 +1,16 @@
 const { composePlugins, withNx } = require('@nx/webpack');
 const { withReact } = require('@nx/react');
+const path = require('path');
 
+const contentRoot = path.resolve(__dirname, './src/content-script');
+const backgroundRoot = path.resolve(__dirname, './src/background-script');
 // Nx plugins for webpack.
 module.exports = composePlugins(withNx(), withReact(), (config) => {
   // Update the webpack config as needed here.
   // e.g. `config.plugins.push(new MyPlugin())`
+  config.entry = {
+    background_script: [`${backgroundRoot}/background.ts`],
+    content_script: [`${contentRoot}/main.tsx`],
+  };
   return config;
 });
